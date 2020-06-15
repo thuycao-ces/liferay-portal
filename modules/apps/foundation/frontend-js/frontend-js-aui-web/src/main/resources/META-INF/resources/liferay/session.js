03670c04029d1a3406adca3ed300bc6d5c92c919
CES-huy-nguyen <huy.nguyen@codeenginestudio.com>
2020-06-15 16:38:52 +0700

    set session state active after close the extend dialog

--- modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
+++ modules/apps/foundation/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
@@ -505,7 +505,7 @@ AUI.add(
 						instance._intervalId = host.registerInterval(
 							function(elapsed, interval, hasWarned, hasExpired, warningMoment, expirationMoment) {
 								if (!hasWarned) {
-									instance._uiSetActivated();
+									host.set('sessionState', 'active', SRC_EVENT_OBJ);
 								}
 								else if (!hasExpired) {
 									if (warningMoment) {
